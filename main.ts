bd0ea8af4e1cb365e9d2784521fbdb8d8ccba083
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-01-09 16:14:39 +0100

    feat: alternate note

--- main.ts
+++ main.ts
@@ -1,11 +1,39 @@
-import { Plugin } from "obsidian";
+import { Notice, Plugin, TFile } from "obsidian";
+
+// add type safety for the undocumented methods
+declare module "obsidian" {
+	interface Workspace { /* eslint-disable-line no-unused-vars */
+		lastOpenFiles: string[];
+	}
+}
 
 export default class grapplingHook extends Plugin {
-
 	async onload() {
 		console.log("🪝 Grappling Hook Plugin loaded.");
+
+		this.addCommand({
+			id: "alternate-note",
+			name: "Switch to Alternate Note",
+			callback: () => this.alternateNote,
+		});
 	}
 
-	async onunload() { console.log("🪝 Grappling Hook Plugin unloaded.") }
+	async onunload() {
+		console.log("🪝 Grappling Hook Plugin unloaded.");
+	}
 
+	// emulate vim's `:buffer #`
+	async alternateNote() {
+		const recentFiles = this.app.workspace.lastOpenFiles;
+		for (const file of recentFiles) {
+			// @ts-ignore, exists() seems to undocumented?
+			const fileExists = await this.app.vault.exists(file); /* eslint-disable-line no-await-in-loop */
+			const altTFile = app.vault.getAbstractFileByPath(file);
+			if (fileExists && altTFile instanceof TFile) {
+				this.app.workspace.getLeaf().openFile(altTFile);
+				return;
+			}
+		}
+		new Notice("No valid recent note exists.");
+	}
 }
