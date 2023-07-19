9c75667e30115b7934d868e5e7ea3d259b165b5f
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-19 20:34:55 +0200

    build: use make

--- release.sh
+++ release.sh
@@ -1,42 +1,33 @@
-#!/bin/zsh
-
+#!/usr/bin/env zsh
 # Release Obsidian Plugin
 # https://forum.obsidian.md/t/using-github-actions-to-release-plugins/7877
 # https://marcus.se.net/obsidian-plugin-docs/publishing/release-your-plugin-with-github-actions
+
+#───────────────────────────────────────────────────────────────────────────────
 
 # ensure relevant files exist
 if [[ ! -f "./manifest.json" ]] ; then
 	echo "manifest.json does not exist yet"
 	exit 1
-fi
-if [[ ! -f "./versions.json" ]] ; then
+elif [[ ! -f "./versions.json" ]] ; then
 	echo "versions.json does not exist yet"
 	exit 1
-fi
-if [[ ! -f "./package.json" ]] ; then
+elif [[ ! -f "./package.json" ]] ; then
 	echo "package.json does not exist yet"
 	exit 1
-fi
-if [[ ! -f "./.github/workflows/release.yml" ]] ; then
+elif [[ ! -f "./.github/workflows/release.yml" ]] ; then
 	echo "/.github/workflows/release.yml does not exist yet"
 	exit 1
 fi
 
+#───────────────────────────────────────────────────────────────────────────────
+
 # Prompt for version number, if not entered
-nextVersion="$*"
 currentVersion=$(grep "version" "./manifest.json" | cut -d\" -f4)
 echo "current version: $currentVersion"
 echo -n "   next version: "
-if [[ -z "$nextVersion" ]]; then
-	read -r nextVersion
-else
-	echo "$nextVersion"
-fi
-echo ""
-
-# Lint
-cd "$(dirname "$0")" || exit 1
-eslint . --fix --ext=ts # to not lint the main.js files
+read -r nextVersion
+echo
 
 # set version number in `manifest.json`
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "manifest.json"
@@ -51,16 +42,11 @@ echo "  \"$nextVersion\": \"$minObsidianVersion\"" >> temp
 echo "}" >> temp
 mv temp versions.json
 
-# update changelog
-echo "- $(date +"%Y-%m-%d")	release $nextVersion" > ./Changelog.md
-git log --pretty=format:"- %ad%x09%s" --date=short | grep -Ev "minor$" | grep -Ev "patch$" | grep -Ev "typos?$" | grep -v "refactoring" | grep -v "Add files via upload" | grep -Ev "\tDelete" | grep -Ev "\tUpdate.*\.md" | sed -E "s/\t\+ /\t/g" >> ./Changelog.md
+#───────────────────────────────────────────────────────────────────────────────
 
 # push the manifest and versions JSONs
-git add -A
-git commit -m "release $nextVersion"
-
-git pull
-git push
+git add -A && git commit -m "release: $nextVersion"
+git pull && git push
 
 # trigger the release action
 git tag "$nextVersion"
