590edd55a36b5ff5d319792fb9e79cf66306ee52
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-20 18:35:33 +0100

    fix: better url regex

--- src/commands/open-first-url-in-file.ts
+++ src/commands/open-first-url-in-file.ts
@@ -1,8 +1,8 @@
 import { Editor, Notice } from "obsidian";
 
 export function openFirstUrlInFile(editor: Editor): void {
-	const urlRegex = /(https?:\/\/[^\s]+)/;
-	const firstUrl = (editor.getValue().match(urlRegex) || [])[0];
+	const urlRegex = /https?:\/\/[^\s)"']+/;
+	const [firstUrl] = editor.getValue().match(urlRegex) || [];
 	if (!firstUrl) {
 		new Notice("No URL found in current file.");
 		return;
