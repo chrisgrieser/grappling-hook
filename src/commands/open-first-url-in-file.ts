5dfbf4fe8fedbafdae74cd109fd5c53c7c639156
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-06 15:12:56 +0200

    chore: update to biome 1.8

--- src/commands/open-first-url-in-file.ts
+++ src/commands/open-first-url-in-file.ts
@@ -1,7 +1,8 @@
 import { Editor, Notice } from "obsidian";
 
+const urlRegex = /https?:\/\/[^\s)"']+/;
+
 export function openFirstUrlInFile(editor: Editor): void {
-	const urlRegex = /https?:\/\/[^\s)"']+/;
 	const [firstUrl] = editor.getValue().match(urlRegex) || [];
 	if (!firstUrl) {
 		new Notice("No URL found in current file.");
